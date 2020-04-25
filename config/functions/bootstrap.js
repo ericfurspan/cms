37211193d92b56fa69ac4260ef8735594545d5de
Eric Furspan <eric.furspan@gmail.com>
2020-04-25 01:59:37 -0400

    setup dev atlas cluster

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,4 +10,7 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/configurations/configurations.html#bootstrap
  */
 
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
+}
 module.exports = () => {};
